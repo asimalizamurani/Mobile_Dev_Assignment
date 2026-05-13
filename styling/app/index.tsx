2b27457f53d938339957044e8553607a1ac5bb22
Asim Ali <asimalizamurani@gmail.com>
2026-05-13 21:14:22 +0500

    react-native second leacture which is about style sheet.

---
+++ styling/app/index.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>HomeScreen by Asim Ali</Text>
+      
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
